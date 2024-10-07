9243d136f9a40c9634e9031fa3212795911a71a4
HaikalAhmadzaky <haikalzaky786@gmail.com>
2024-10-07 09:54:23 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/biodataform/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.biodataform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class Baseurl {
+  static String tambah = "http://192.168.43.78/biodata/create.php";
+  static String data = "http://192.168.43.78/biodata/list.php";
+  static String lihat = "http://192.168.43.78/biodata/details.php";
+  static String edit = "http://192.168.43.78/biodata/update.php";
+  static String hapus = "http://192.168.43.78/biodata/delete.php";
+}
